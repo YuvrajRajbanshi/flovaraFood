af707d5cb8b6f82170075a8be383d33a7bb5f43a
Yuvi <singhraj0461@gmail.com>
2024-01-01 16:24:04 +0530

    project setup

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+
+
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <h1> Hello dear  this is yuvraj Singh</h1>
+    </>
+  )
+}
+
+export default App
